4b2f629906078903834a72f96212444985ac466a
Margarita Khokhlova <MKhokhlova@ign.fr>
2020-03-21 23:29:16 +0100

    contrastive loss main modified

--- contrastive_loss.py
+++ contrastive_loss.py
@@ -51,35 +51,12 @@ class ContrastiveLossLayer(tf.keras.layers.Layer):
         self.add_loss(loss)
         return loss
 
-
-#
-# @tf.function
-# def contrastive_loss(xi, xj,  tau=1, normalize=False):
-#         x = tf.concat([xi, xj], axis=0)
-#         sim_mat = tf.tensordot(x, tf.transpose(x), axes = 1)
-#         if normalize:
-#             sim_mat_denom = tf.keras.backend.dot(tf.keras.backend.norm(x, dim=1).unsqueeze(1), tf.keras.backend.norm.norm(x, dim=1).unsqueeze(1).T)
-#             sim_mat = sim_mat / sim_mat_denom.clamp(min=1e-16)
-#
-#         sim_mat = tf.math.exp(sim_mat /tau)
-#
-#         # top
-#         if normalize:
-#             sim_mat_denom = tf.keras.backend.norm(xi, dim=1) * tf.keras.backend.norm(xj, axis=1)
-#             sim_match = tf.keras.backend.exp(tf.keras.backend.sum(xi * xj, axis=-1) / sim_mat_denom / tau)
-#         else:
-#             sim_match = tf.math.exp(tf.keras.backend.sum(xi * xj, axis=-1) / tau)
-#
-#         sim_match = tf.concat([sim_match, sim_match], axis=0)
-#
-#         norm_sum = tf.math.exp(tf.ones(tf.shape(x)[0]) / tau)
-#         return tf.reduce_mean(tf.cast(tf.math.reduce_mean(-tf.math.log(sim_match / (tf.reduce_sum(sim_mat, axis=-1) - norm_sum))), tf.dtypes.float32))
-
 if __name__ == '__main__':
     random.seed(30)
     batch_size = 2
     feature_size = 16
     x =np.random.rand(12, 256)
+    # incomment for a static input test
     # x = np.asarray([[0.1, 0.1, 0.2, 0.3, 0.4],  # 1st row
     #                 [0.2, 0.6, 0.7, 0.8, 0.9],  # 2nd row
     #                 [0.3, 0.6, 0.7, 0.8, 0.9],  # 3rd row
